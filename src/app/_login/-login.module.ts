b95e9d7c007fb7b2ff9ea5071878f29ad8cbf550
SaiKumar9889 <48714889+SaiKumar9889@users.noreply.github.com>
2023-06-22 13:48:01 +0530

    Flex

--- src/app/_login/-login.module.ts
+++ src/app/_login/-login.module.ts
@@ -1,21 +1,21 @@
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginRouteModule } from './_login.route.module';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginRouteModule } from "./_login.route.module";
+import { RegistrationComponent } from "./registration/registration.component";
+import { LoginComponent } from "./login/login.component";
 
-
+import { MaterialModule } from "../material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-    RegistrationComponent,
-    LoginComponent
-  ],
+  declarations: [RegistrationComponent, LoginComponent],
   imports: [
     CommonModule,
     FormsModule,
     LoginRouteModule,
-  ]
+    MaterialModule,
+    FlexLayoutModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
