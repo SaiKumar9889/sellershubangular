30be1734a6fdaba7e16552c39be79ce78c0edd37
SaiKumar9889 <48714889+SaiKumar9889@users.noreply.github.com>
2023-06-22 13:10:18 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   {
     path: 'login',
-    loadChildren: () => import('./-login/-login.module').then(module => module.LoginModule)
+    loadChildren: () => import('./_login/-login.module').then(module => module.LoginModule)
   },
 ];
 
